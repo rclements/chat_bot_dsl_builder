8a06fde6155fd809a46d5833ed12e0a326d6f004
rclements <rclements@isotope11.com>
2015-08-15 16:04:34 -0500

    Add modal for selecting if condition

--- app/ast_builder/ast_builder.js
+++ app/ast_builder/ast_builder.js
@@ -11,52 +11,56 @@ angular.module('chatbotApp.AstBuilder', ['ngRoute'])
   });
 }])
 
+
 .controller('AstBuilderCtrl', ['$scope', function($scope) {
-  var templates = {
-    barebonesIf: ["tuple",
-                   ["atom", "if"],
-                   ["placeholder"],
-                   ["placeholder"]
-                 ],
-    ifWithCondition: ["tuple",
-                       ["atom", "if"],
-                       ["tuple",
-                         ["atom", "input"],
-                         ["atom", "contains"],
-                         ["string", ":tableflip:"]
+  $scope.condition = ["placeholder"];
+  $scope.templates = function(){
+    return {
+      barebonesIf: ["tuple",
+                     ["atom", "if"],
+                     $scope.condition,
+                     ["placeholder"]
+                   ],
+      ifWithCondition: ["tuple",
+                         ["atom", "if"],
+                         $scope.condition,
+                         ["placeholder"]
                        ],
-                       ["placeholder"]
-                     ],
-    ifWithConditionAndAction: ["tuple",
-                       ["atom", "if"],
-                       ["tuple",
-                         ["atom", "input"],
-                         ["atom", "contains"],
-                         ["string", ":tableflip:"]
-                       ],
-                       ["response", "(╯°□°）╯︵ ┻━┻"]
-                     ]
+      ifWithConditionAndAction: ["tuple",
+                         ["atom", "if"],
+                         ["tuple",
+                           ["atom", "input"],
+                           ["atom", "contains"],
+                           ["string", ":tableflip:"]
+                         ],
+                         ["response", "(╯°□°）╯︵ ┻━┻"]
+                       ]
+    };
   };
+
 
   $scope.availableIfConditions = ["input contains tableflip"];
 
   $scope.numbers = [1, 2, 3];
   $scope.createBlock = function(type){
     if(type === 'if'){
-      $scope.ast = templates.barebonesIf;
+      $scope.ast = $scope.templates().barebonesIf;
     }
   };
   $scope.astIsEmpty = function(){
     return angular.equals($scope.ast, {});
   };
   $scope.astIsBarebonesIf = function(){
-    return angular.equals($scope.ast, templates.barebonesIf);
+    return angular.equals($scope.ast, $scope.templates().barebonesIf);
+  };
+  $scope.astIsBarebonesIfWithPlaceholder = function(){
+    return angular.equals($scope.condition, ["placeholder"]);
   };
   $scope.astIsIfWithCondition = function(){
-    return angular.equals($scope.ast, templates.ifWithCondition);
+    return angular.equals($scope.ast, $scope.templates().ifWithCondition);
   };
   $scope.astIsIfWithConditionAndAction = function(){
-    return angular.equals($scope.ast, templates.ifWithConditionAndAction);
+    return angular.equals($scope.ast, $scope.templates().ifWithConditionAndAction);
   };
   $scope.getIfConditions = function(){
     // open modal
@@ -64,13 +68,24 @@ angular.module('chatbotApp.AstBuilder', ['ngRoute'])
       .modal('show');
     //return $scope.availableIfConditions;
   };
+  $scope.setIfCondition = function(condition) {
+    $scope.condition =  ["tuple",
+                         ["atom", "input"],
+                         ["atom", "contains"],
+                         ["string", ":tableflip:"]
+                       ];
+    $scope.ast = $scope.templates().ifWithCondition;
+    angular.element('#ifConditionsModal')
+      .modal('hide');
+  };
   $scope.addIfCondition = function(){
-    $scope.ast = templates.ifWithCondition;
+    $scope.ast = $scope.templates().ifWithCondition;
   };
   $scope.addIfAction = function(){
-    $scope.ast = templates.ifWithConditionAndAction;
+    $scope.ast = $scope.templates().ifWithConditionAndAction;
   };
   $scope.getIfCondition = function(){
+    console.log($scope.ast);
     return $scope.ast[2][3][1];
   };
   $scope.ast = {};
