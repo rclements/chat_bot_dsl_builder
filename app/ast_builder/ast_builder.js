cd7ab0d82e829fde2d13f24276e957b9adc16172
rclements <rclements@isotope11.com>
2015-08-15 15:03:28 -0500

    Add some boilerplate walkthrough
    Start thinking through how the UI will work...

--- app/ast_builder/ast_builder.js
+++ app/ast_builder/ast_builder.js
@@ -18,7 +18,25 @@ angular.module('chatbotApp.AstBuilder', ['ngRoute'])
                    ["atom", "if"],
                    ["placeholder"],
                    ["placeholder"]
-                 ]
+                 ],
+    ifWithCondition: ["tuple",
+                       ["atom", "if"],
+                       ["tuple",
+                         ["atom", "input"],
+                         ["atom", "contains"],
+                         ["string", ":tableflop:"]
+                       ],
+                       ["placeholder"]
+                     ],
+    ifWithConditionAndAction: ["tuple",
+                       ["atom", "if"],
+                       ["tuple",
+                         ["atom", "input"],
+                         ["atom", "contains"],
+                         ["string", "filthy"]
+                       ],
+                       ["response", "(╯°□°）╯︵ ┻━┻"]
+                     ]
   };
 
   $scope.numbers = [1, 2, 3];
@@ -32,6 +50,21 @@ angular.module('chatbotApp.AstBuilder', ['ngRoute'])
   };
   $scope.astIsBarebonesIf = function(){
     return angular.equals($scope.ast, templates.barebonesIf);
+  };
+  $scope.astIsIfWithCondition = function(){
+    return angular.equals($scope.ast, templates.ifWithCondition);
+  };
+  $scope.astIsIfWithConditionAndAction = function(){
+    return angular.equals($scope.ast, templates.ifWithConditionAndAction);
+  };
+  $scope.addIfCondition = function(){
+    $scope.ast = templates.ifWithCondition;
+  };
+  $scope.addIfAction = function(){
+    $scope.ast = templates.ifWithConditionAndAction;
+  };
+  $scope.getIfCondition = function(){
+    return $scope.ast[2][3][1];
   };
   $scope.ast = {};
 });
