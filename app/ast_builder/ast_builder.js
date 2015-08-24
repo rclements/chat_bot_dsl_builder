e7a221aa2837c16e9db8b084df5d7d71232d6f94
Josh Adams <josh.rubyist@gmail.com>
2015-08-24 18:05:50 -0500

    Add ast-if and ast-element directives

--- app/ast_builder/ast_builder.js
+++ app/ast_builder/ast_builder.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('loaded ast builder...');
-
 angular.module('chatbotApp.AstBuilder', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -22,23 +20,6 @@ angular.module('chatbotApp.AstBuilder', ['ngRoute'])
                  ];
   };
 
-  $scope.availableIfConditions = [
-    [
-      ["atom", "input"],
-      ["atom", "contains"],
-      ["string", ":tableflip:"]
-    ],
-    [
-      ["atom", "input"],
-      ["atom", "contains"],
-      ["string", "filthy"]
-    ]
-  ];
-
-  $scope.availableIfActions = [
-    ["response", "(╯°□°）╯︵ ┻━┻"]
-  ];
-
   $scope.createBlock = function(type){
     if(type === 'if'){
       $scope.setAst();
@@ -50,44 +31,104 @@ angular.module('chatbotApp.AstBuilder', ['ngRoute'])
   $scope.astIsBarebonesIfWithPlaceholder = function(){
     return angular.equals($scope.condition, ["placeholder"]);
   };
-  $scope.getIfConditions = function(){
-    // open modal
-    angular.element('#ifConditionsModal')
-      .modal('show');
-    //return $scope.availableIfConditions;
-  };
-  $scope.setIfCondition = function(condition) {
-    $scope.condition =  ["tuple"].concat(condition);
-    $scope.setAst();
-    angular.element('#ifConditionsModal')
-      .modal('hide');
-  };
-  $scope.getIfActions = function() {
-    // open modal
-    angular.element('#ifActionsModal')
-      .modal('show');
-  };
-  $scope.setIfAction = function(action) {
-    console.log("setting ", action);
-    $scope.action = action;
-    $scope.setAst();
-    angular.element('#ifActionsModal')
-      .modal('hide');
-  };
-  $scope.addIfCondition = function(){
-    $scope.setAst();
-  };
-  $scope.addIfAction = function(){
-    $scope.setAst();
-  };
-  $scope.getIfCondition = function(){
-    return $scope.ast[2][3][1];
-  };
-  $scope.astIfConditionIsPlaceholder = function(){
-    return angular.equals($scope.ast[2], ["placeholder"]);
-  };
-  $scope.astIfActionIsPlaceholder = function(){
-    return angular.equals($scope.ast[3], ["placeholder"]);
-  };
   $scope.ast = {};
-}]);
+}])
+
+.directive("astIf", [function(){
+  function link(scope, element, attrs) {
+    scope.getConditions = function(){
+      // open modal
+      angular.element('.ifConditionsModal', element)
+        .modal('show');
+    };
+    scope.updateAst = function() {
+      scope.ast = [
+        "tuple",
+        ["atom", "if"],
+        scope.condition,
+        scope.action
+      ];
+    }
+    scope.setCondition = function(condition) {
+      scope.condition =  ["tuple"].concat(condition);
+      scope.updateAst();
+      angular.element('.ifConditionsModal')
+        .modal('hide');
+    };
+    scope.getActions = function() {
+      // open modal
+      angular.element('.ifActionsModal', element)
+        .modal('show');
+    };
+    scope.setAction = function(action) {
+      console.log("setting ", action);
+      $scope.action = action;
+      scope.updateAst();
+      angular.element('.ifActionsModal')
+        .modal('hide');
+    };
+  }
+
+  return {
+    restrict: "E",
+    scope: {
+      ast: "="
+    },
+    templateUrl: "./ast_builder/ast_if.html",
+    link: link,
+    controller: function($scope){
+      $scope.condition = "placeholder";
+      $scope.action = "placeholder";
+      $scope.availableIfConditions = [
+        [
+          ["atom",   "contains"],
+          ["var",    "input"],
+          ["string", ":tableflip:"]
+        ],
+        [
+          ["atom",   "contains"],
+          ["var",    "input"],
+          ["string", "filthy"]
+        ]
+      ];
+
+      $scope.availableIfActions = [
+        ["response", "(╯°□°）╯︵ ┻━┻"]
+      ];
+
+      $scope.addCondition = function(){
+        $scope.setAst();
+      };
+      $scope.addAction = function(){
+        $scope.setAst();
+      };
+      $scope.getCondition = function(){
+        console.log("getCondition");
+        console.log($scope.ast);
+        return $scope.ast[2][3][1];
+      };
+      $scope.conditionIsPlaceholder = function(){
+        return angular.equals($scope.ast[2], ["placeholder"]);
+      };
+      $scope.actionIsPlaceholder = function(){
+        return angular.equals($scope.ast[3], ["placeholder"]);
+      };
+    }
+  }
+}])
+
+.directive("astElement", [function(){
+  return {
+    scope: {
+      ast: "="
+    },
+    templateUrl: "./ast_builder/ast_element.html",
+    controller: function($scope){
+      $scope.astIsIf = function(){
+        return angular.equals($scope.ast[0], "tuple") &&
+          angular.equals($scope.ast[1], ["atom", "if"]);
+      };
+    }
+  }
+}])
+;
